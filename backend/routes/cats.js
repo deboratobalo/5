1b57e0fd60427d2156c1990952a654753c4653f6
Débora <deboralobato40@tutanota.com>
2024-04-02 18:54:38 -0300

    fix: corrigindo rotas do crud

--- backend/routes/cats.js
+++ backend/routes/cats.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { getCats } from "../controllers/cat.js"
+import { getCats, addCat, updateCat } from "../controllers/cat.js"
 
 const router = express.Router()
 
 router.get("/", getCats)
 
+router.post("/", addCat)
+
+router.put("/:id", updateCat)
 export default router
 
