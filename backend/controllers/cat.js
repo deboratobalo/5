1da026466d967824954391506fa22f36708afbf5
Débora <deboralobato40@tutanota.com>
2024-04-02 18:50:12 -0300

    feat: crud adicionado e finalizado

--- backend/controllers/cat.js
+++ backend/controllers/cat.js
@@ -11,25 +11,31 @@ export const getCats = (_, res) => {
 
 }
 
+
 export const addCat = (req, res) => {
-  const q = "insert into gatos(`nome`, `idade`, `microchip`) values(?)"
+  const q = 
+  "insert into gatos(`nome`, `idade`, `microchip`) values(?)"
+
+  const values = [
+    req.body.nome,
+    req.body.idade,
+    req.body.microchip,
+  ]
+  
+  db.query(q, [values], (error) => {
+    if (error) return res.json(error)
+  
+    return res.status(200).json("Gato cadastrado com sucesso!")
+  })
+
 }
 
-const values = [
-  req.body.nome,
-  req.body.idade,
-  req.body.microchip,
-]
-
-db.query(q, [values], (error) => {
-  if (error) return res.json(error)
-
-  return res.status(200).json("Gato cadastrado com sucesso!")
-})
 
 
 export const updateCat = (req, res) => {
   const q = "update gatos set `nome` = ?, `idade` = ?, `microchip = ? where `id` = ?"
+
+  
 const values = [
   req.body.nome,
   req.body.idade,
